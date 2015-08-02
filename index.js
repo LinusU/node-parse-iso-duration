463592060a47452c54cb718d4f4d7d6e4f9e104e
Linus Unnebäck <linus@folkdatorn.se>
2015-08-02 11:46:04 +0200

    coding style: standard

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
+var week = /^P([0-9]+W)$/
+var dateTime = /^P(([0-9]+Y)?([0-9]+M)?([0-9]+D)?)?(T([0-9]+H)?([0-9]+M)?([0-9]+S)?)?$/
 
-var week = /^P([0-9]+W)$/;
-var dateTime = /^P(([0-9]+Y)?([0-9]+M)?([0-9]+D)?)?(T([0-9]+H)?([0-9]+M)?([0-9]+S)?)?$/;
-
-function extractInt(str) {
-  return parseInt(str.substring(0, str.length - 1), 10);
+function extractInt (str) {
+  return parseInt(str.substring(0, str.length - 1), 10)
 }
 
-function parsePart(mode, str) {
-  if (!str) { return 0; }
+function parsePart (mode, str) {
+  if (!str) return 0
 
-  var n = extractInt(str);
-  var id = mode + ' ' + str[str.length - 1];
+  var n = extractInt(str)
+  var id = mode + ' ' + str[str.length - 1]
 
-  if (n === 0) { return 0; }
+  if (n === 0) return 0
 
   switch (id) {
-    case 'time S': return n * 1000;
-    case 'time M': return n * 60000;
-    case 'time H': return n * 3600000;
-    case 'date D': return n * 86400000;
-    case 'week W': return n * 604800000;
+    case 'time S': return n * 1000
+    case 'time M': return n * 60000
+    case 'time H': return n * 3600000
+    case 'date D': return n * 86400000
+    case 'week W': return n * 604800000
     case 'date M':
     case 'date Y':
-      throw new Error('Ambiguous duration');
+      throw new Error('Ambiguous duration')
   }
-
 }
 
-function parseIsoDuration(str) {
+function parseIsoDuration (str) {
+  var m
 
-  var m;
-
-  if (m = week.exec(str)) {
-    return parsePart('week', m[1]);
+  m = week.exec(str)
+  if (m) {
+    return parsePart('week', m[1])
   }
 
-  if (m = dateTime.exec(str)) {
+  m = dateTime.exec(str)
+  if (m) {
     return (
       parsePart('date', m[2]) +
       parsePart('date', m[3]) +
@@ -43,10 +42,10 @@ function parseIsoDuration(str) {
       parsePart('time', m[6]) +
       parsePart('time', m[7]) +
       parsePart('time', m[8])
-    );
+    )
   }
 
-  throw new Error('Invalid duration');
+  throw new Error('Invalid duration')
 }
 
-module.exports = parseIsoDuration;
+module.exports = parseIsoDuration
--- test/basic.js
+++ test/basic.js
@@ -1,78 +1,71 @@
+/* eslint-env mocha */
 
-var assert = require('assert');
-var parseIsoDuration = require('../');
+var assert = require('assert')
+var parseIsoDuration = require('../')
 
 describe('parseIsoDuration', function () {
-
   it('should parse durations', function () {
+    assert.equal(parseIsoDuration('P0W'), 0)
+    assert.equal(parseIsoDuration('P1W'), 1 * 604800000)
+    assert.equal(parseIsoDuration('P5W'), 5 * 604800000)
+    assert.equal(parseIsoDuration('P10W'), 10 * 604800000)
 
-    assert.equal(parseIsoDuration('P0W'), 0);
-    assert.equal(parseIsoDuration('P1W'), 1 * 604800000);
-    assert.equal(parseIsoDuration('P5W'), 5 * 604800000);
-    assert.equal(parseIsoDuration('P10W'), 10 * 604800000);
+    assert.equal(parseIsoDuration('PT0S'), 0)
+    assert.equal(parseIsoDuration('PT1S'), 1 * 1000)
+    assert.equal(parseIsoDuration('PT5S'), 5 * 1000)
+    assert.equal(parseIsoDuration('PT10S'), 10 * 1000)
 
-    assert.equal(parseIsoDuration('PT0S'), 0);
-    assert.equal(parseIsoDuration('PT1S'), 1 * 1000);
-    assert.equal(parseIsoDuration('PT5S'), 5 * 1000);
-    assert.equal(parseIsoDuration('PT10S'), 10 * 1000);
+    assert.equal(parseIsoDuration('PT0M'), 0)
+    assert.equal(parseIsoDuration('PT1M'), 1 * 60000)
+    assert.equal(parseIsoDuration('PT5M'), 5 * 60000)
+    assert.equal(parseIsoDuration('PT10M'), 10 * 60000)
 
-    assert.equal(parseIsoDuration('PT0M'), 0);
-    assert.equal(parseIsoDuration('PT1M'), 1 * 60000);
-    assert.equal(parseIsoDuration('PT5M'), 5 * 60000);
-    assert.equal(parseIsoDuration('PT10M'), 10 * 60000);
+    assert.equal(parseIsoDuration('PT0H'), 0)
+    assert.equal(parseIsoDuration('PT1H'), 1 * 3600000)
+    assert.equal(parseIsoDuration('PT5H'), 5 * 3600000)
+    assert.equal(parseIsoDuration('PT10H'), 10 * 3600000)
 
-    assert.equal(parseIsoDuration('PT0H'), 0);
-    assert.equal(parseIsoDuration('PT1H'), 1 * 3600000);
-    assert.equal(parseIsoDuration('PT5H'), 5 * 3600000);
-    assert.equal(parseIsoDuration('PT10H'), 10 * 3600000);
+    assert.equal(parseIsoDuration('PT0H2M'), 2 * 60000)
+    assert.equal(parseIsoDuration('PT1H5M'), 1 * 3600000 + 5 * 60000)
+    assert.equal(parseIsoDuration('PT5H10S'), 5 * 3600000 + 10 * 1000)
+    assert.equal(parseIsoDuration('PT10H40M23S'), 10 * 3600000 + 40 * 60000 + 23 * 1000)
 
-    assert.equal(parseIsoDuration('PT0H2M'), 2 * 60000);
-    assert.equal(parseIsoDuration('PT1H5M'), 1 * 3600000 + 5 * 60000);
-    assert.equal(parseIsoDuration('PT5H10S'), 5 * 3600000 + 10 * 1000);
-    assert.equal(parseIsoDuration('PT10H40M23S'), 10 * 3600000 + 40 * 60000 + 23 * 1000);
+    assert.equal(parseIsoDuration('P0D'), 0 * 86400000)
+    assert.equal(parseIsoDuration('P1D'), 1 * 86400000)
+    assert.equal(parseIsoDuration('P5D'), 5 * 86400000)
+    assert.equal(parseIsoDuration('P10D'), 10 * 86400000)
 
-    assert.equal(parseIsoDuration('P0D'), 0 * 86400000);
-    assert.equal(parseIsoDuration('P1D'), 1 * 86400000);
-    assert.equal(parseIsoDuration('P5D'), 5 * 86400000);
-    assert.equal(parseIsoDuration('P10D'), 10 * 86400000);
-
-    assert.equal(parseIsoDuration('P0DT30M'), 0 * 86400000 + 30 * 60000);
-    assert.equal(parseIsoDuration('P10DT30S'), 10 * 86400000 + 30 * 1000);
-    assert.equal(parseIsoDuration('P12DT28M'), 12 * 86400000 + 28 * 60000);
-    assert.equal(parseIsoDuration('P14DT26H'), 14 * 86400000 + 26 * 3600000);
-
-  });
+    assert.equal(parseIsoDuration('P0DT30M'), 0 * 86400000 + 30 * 60000)
+    assert.equal(parseIsoDuration('P10DT30S'), 10 * 86400000 + 30 * 1000)
+    assert.equal(parseIsoDuration('P12DT28M'), 12 * 86400000 + 28 * 60000)
+    assert.equal(parseIsoDuration('P14DT26H'), 14 * 86400000 + 26 * 3600000)
+  })
 
   it('should throw on garbage', function () {
-
     var invalid = [
       'Jsjd kals',
       'PTJKS',
       'PT123123',
       ' gdjfkds',
       'Hello world'
-    ];
+    ]
 
     invalid.forEach(function (str) {
-      assert.throws(parseIsoDuration.bind(null, str), Error, 'Invalid duration');
-    });
-
-  });
+      assert.throws(parseIsoDuration.bind(null, str), Error, 'Invalid duration')
+    })
+  })
 
   it('should throw on ambiguous durations', function () {
-
     var ambiguous = [
       'P10Y',
       'P5MT10M',
       'P0Y1M',
       'P1Y0MT24H',
       'P8Y20M10D'
-    ];
+    ]
 
     ambiguous.forEach(function (str) {
-      assert.throws(parseIsoDuration.bind(null, str), Error, 'Ambiguous duration');
-    });
-
-  });
-
-});
+      assert.throws(parseIsoDuration.bind(null, str), Error, 'Ambiguous duration')
+    })
+  })
+})
